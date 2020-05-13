0c840af7eb30e21150334fb5fb42a6197fd41a05
Dan Matthews <dan@cryptobuilders.io>
2020-05-13 14:03:08 +0300

    adds env var for discord alert addresses

--- src/adapters/discord.js
+++ src/adapters/discord.js
@@ -1,10 +1,17 @@
 import fetch from 'node-fetch';
 
+const alertId = process.env.DISCORD_ALERT_ID;
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 
-export default async (message) => {
+export default async (message, shouldAlert = false) => {
+  let msg = message;
+
+  if (shouldAlert && alertId) {
+    msg = `<@${alertId}> ALERT!! - ${msg}`;
+  }
+
   if (webhookUrl) {
-    const body = JSON.stringify({ content: message });
+    const body = JSON.stringify({ content: msg });
     const headers = { 'Content-Type': 'application/json' };
     const response = await fetch(webhookUrl, { method: 'POST', body, headers });
     console.log('STATUS', response.status);
